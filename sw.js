f09b8d5f940576f7a522dcc9e894b30c0b27e59b
ribocg-a11y <ribocg@gmail.com>
2026-05-28 19:19:19 -0300

    chore(sw): v1.6.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// MOVI KIDS — Service Worker v1.6.4
-const CACHE = 'movikids-v1.6.4';
+// MOVI KIDS — Service Worker v1.6.5
+const CACHE = 'movikids-v1.6.5';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
