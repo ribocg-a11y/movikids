dc2b186b78d47f16f5ae85c78bebf234430b1751
ribocg-a11y <antonio.luis.vieira.nj@gmail.com>
2026-05-31 19:40:16 -0300

    Align app and service worker version v1.6.46

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// MOVI KIDS - Service Worker 1.6.45
+// MOVI KIDS - Service Worker 1.6.46
 // Hotfix: nao manter cache persistente do app. Rede sempre primeiro.
-const SW_VERSION = '1.6.45';
+const SW_VERSION = '1.6.46';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
