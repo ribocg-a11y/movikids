95fbfabf1c4a1fded322299ba3320262e62852be
ribocg-a11y <antonio.luis.vieira.nj@gmail.com>
2026-05-31 19:50:36 -0300

    Fix local sync broadcast alias v1.6.47

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// MOVI KIDS - Service Worker 1.6.46
+// MOVI KIDS - Service Worker 1.6.47
 // Hotfix: nao manter cache persistente do app. Rede sempre primeiro.
-const SW_VERSION = '1.6.46';
+const SW_VERSION = '1.6.47';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
